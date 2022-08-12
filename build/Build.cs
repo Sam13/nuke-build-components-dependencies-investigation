814e03619b4fcd135e32faf8ab15308f64861ef1
Sam <fertilefields@tutamail.com>
2022-08-12 16:12:41 +0200

    Add build components

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,60 @@
 using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
-class Build : NukeBuild
+class Build : NukeBuild,
+    IRestoreDotNet,
+    ICompileDotNet
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => ((ICompile)x).Compile);
+}
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+interface IRestore : INukeBuild
+{
+    Target Restore => _ => _;
+}
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
-
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
-
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
-
+interface ICompile : INukeBuild
+{
     Target Compile => _ => _
-        .DependsOn(Restore)
+        .TryDependsOn<IRestore>();
+}
+
+interface IRestoreNuGetCli : IRestore
+{
+    Target IRestore.Restore => _ => _
+        .Inherit<IRestore>()
         .Executes(() =>
         {
+            Console.WriteLine("nuget.exe restore");
         });
+}
 
+interface IRestoreDotNet : IRestore
+{
+    Target IRestore.Restore => _ => _
+        .Inherit<IRestore>()
+        .Executes(() =>
+        {
+            Console.WriteLine("dotnet restore");
+        });
+}
+
+interface ICompileMsBuild : ICompile
+{
+    Target ICompile.Compile => _ => _
+        .Inherit<ICompile>()
+        .Executes(() =>
+        {
+            Console.WriteLine("msbuild.exe sln");
+        });
+}
+
+interface ICompileDotNet : ICompile
+{
+    Target ICompile.Compile => _ => _
+        .Inherit<ICompile>()
+        .Executes(() =>
+        {
+            Console.WriteLine("dotnet build");
+        });
 }
